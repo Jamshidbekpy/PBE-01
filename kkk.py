f160fbae43541ab3dc0037bcfbd542ef2d165d69
Jamshidbekpy <jamshidbekdev04@gmail.com>
2026-05-07 19:58:17 +0500

    message

--- kkk.py
+++ kkk.py
@@ -1,4 +1,10 @@
 print("Amrillo va Jamshidbek bugun darsda yo'q")
 print("Hello World")
 print("Jamshidbek Shodibekov")
+
 print("This is my first program")
+
+
+def add(num1, num2):
+    return num1 + num2
+
